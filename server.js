75e21a426e6410097365443e1eb71c5122e187c0
kunal-gupta-uix <kunal@uixlabs.in>
2025-04-22 12:10:30 +0530

    Implemented auth functionality : signup, login and middleware

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {connectDB} = require('./config/db');
+const authRouter = require('./routes/authRoutes');
 const { sequelize } = require('./config/db');
 const {User, Project, ProjectMember,Task} = require('./models');
 
 // Sync models with DB
-sequelize.sync({ alter: true })  // or use { force: true } during development to drop and recreate
+sequelize.sync({ force: true })  // or use { force: true } during development to drop and recreate
   .then(() => {
     console.log("All models synced with the database.");
   })
@@ -17,15 +18,18 @@ require('dotenv').config(); // Load environment variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.json()); // Middleware to parse json in requests
 
-//connect to mysql database
+//connect to mysql database  
 connectDB();
 
 //basic/default route
 app.get('/', (req, res)=>{
     res.send('Welcome to the task manager API');
 });
+
+app.use('/auth', authRouter);
 
 // start the server
 app.listen(PORT, ()=>{
