c258665990c6812b5898272b6ca581bd8da461fd
kunal-gupta-uix <kunal@uixlabs.in>
2025-04-22 18:01:55 +0530

    dependencies added for developing otp services

---
+++ utils/generateOTP.js
@@ -0,0 +1,8 @@
+// utils/generateOTP.js
+const generateOTP = () => {
+    const otp = Math.floor(100000 + Math.random() * 900000); // 6-digit OTP
+    return otp.toString();
+  };
+  
+  module.exports = generateOTP;
+  
